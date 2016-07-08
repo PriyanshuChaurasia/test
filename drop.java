672dd3ede91dcffc92ad180372102796a23b55ab
PriyanshuChaurasia <priyanshuc1984@gmail.com>
2016-07-08 23:49:48 +0530

    Add files via upload

---
+++ drop.java
@@ -0,0 +1,64 @@
+package select;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class drop {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		WebElement e=driver.findElement(By.linkText("Dropdown"));
+		e.click();
+		
+	
+		
+		WebElement element=driver.findElement(By.id("dropdown")); 
+		
+		
+		Select sel=new Select(element);
+		sel.selectByIndex(2);
+		 Thread.sleep(3000);
+		//String selvalue= sel.getFirstSelectedOption().getText();
+		//System.out.println("selected value=="+selvalue);
+		//sel.selectByValue("1");
+		 
+		// Actions action=new Actions(driver);
+		//action.moveToElement(e).build().perform();
+		
+		
+		
+	/*	List<WebElement> options=sel.getOptions();
+		
+		for(WebElement option:options )
+		{
+		
+			if(option.equals(val))
+			{
+				option.click();
+				Thread.sleep(2000);
+			}
+			//System.out.println(option);
+		}*/
+		
+		//driver.close();
+		//driver.quit();
+		
+		
+		
+		
+	}
+
+}
